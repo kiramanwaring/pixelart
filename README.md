d7ba1524ae4ba8c2abf38bcd6957ee31bfd62155
Michael C Manwaring <Michael.c.manwaring@gmail.com>
2018-02-20 13:43:10 -0500

    feat: add randomizer

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Pixel Art Maker Project
 
+This is the Final project for the [Grow With Google Scholarship Challenge](https://www.udacity.com/grow-with-google). This version has expanded significantly on the original assignment, if you would like to see the project as specified, check it out on [Codepen](https://codepen.io/MichaelManwaring/pen/BYrGzr).
 
--- designs.js
+++ designs.js
@@ -4,19 +4,18 @@ const gridColor=$('#colorPicker');
 const gridSize=[$("#inputHeight"), $("#inputWeight")];
 console.log(gridSize)
 // When size is submitted by the user, call makeGrid()
+
 const pixelCanvas=$('#pixelCanvas');
 
-
 function makeGrid() {
-// Your code goes here!
 	pixelCanvas.empty();
-	for (var i =  gridSize[0].val(); i > 0; i--) {
+	for (let i =  gridSize[0].val(); i > 0; i--) {
 		pixelCanvas.append('<tr></tr>');
-		for (var j = gridSize[1].val(); j > 0; j--) {
-			pixelCanvas.children().last().append("<td class='color-pixel'></td>");
-		};
-	};
-};
+		for (let j = gridSize[1].val(); j > 0; j--) {
+			pixelCanvas.children().last().append("<td></td>");
+		}
+	}
+}
 
 $("#sizePicker").submit(function(e) {
 	makeGrid();
@@ -24,10 +23,43 @@ $("#sizePicker").submit(function(e) {
 });
 
 
-$(makeGrid());
-
 $("#pixelCanvas").click(function(e) {
-	let pixelToChange= $(e.target)
+	console.log('click');
+	let pixelToChange = $(e.target);
 	// if the click is dragged across the grid, this function prevents the <tr> from changing color
-	pixelToChange.is('td') ? pixelToChange.css('background-color', $('#colorPicker').val()) : console.log('dragged click');
+	if (pixelToChange.is('td')){
+		pixelToChange.css('background-color', nextColor());
+	}else if (pixelToChange.is('tr') && $('#randomizer').is(':checked')){
+		console.log('row click');
+		pixelToChange.children().each(function() {
+			$(this).css('background-color', nextColor());
+		})
+	}else if ($('#randomizer').is(':checked')){
+		console.log('table click');
+		pixelToChange.children().children().each(function() {
+			$(this).css('background-color', nextColor());
+		})
+	}
 });
+
+function nextColor() {
+	if ($('#randomizer').is(':checked')) {
+		$('#colorPicker').val(randomColor());
+	}
+	return $('#colorPicker').val();
+}
+
+function randomColor() {
+	let hexCode = "#";
+	for(i=0; i<3; i++){
+		hexCode+=randomHexFragment();
+	}
+    return hexCode
+}
+
+function randomHexFragment() {
+    var fragment = (Math.floor(Math.random() * 255).toString(16));
+    return fragment.length == 1 ? "0" + fragment : fragment;
+}
+
+$(makeGrid());
