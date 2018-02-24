4d60b9c57b171f51ee252210577765146fe21b14
Michael C Manwaring <Michael.c.manwaring@gmail.com>
2018-02-24 16:58:42 -0500

    implement secret menu, mobile disable & randomPicker

--- designs.js
+++ designs.js
@@ -1,8 +1,12 @@
 // Select color input
 const gridColor=$('#colorPicker');
 // Select size input
-const gridSize=[$("#inputHeight"), $("#inputWeight")];
-console.log(gridSize)
+const gridHeight=$("#inputHeight")
+const gridStretch=$("#inputStretch");
+const stretchTable=[0.618, 1, 1.25, 1.618, 2]
+function gridWidth() {
+	return Math.round(stretchTable[gridStretch.val()-1]*gridHeight.val());
+}
 // When size is submitted by the user, call makeGrid()
 
 const pixelCanvas=$('#pixelCanvas');
@@ -10,20 +14,21 @@ let drawingToggle=true;
 
 function makeGrid() {
 	pixelCanvas.empty();
-	for (let i =  gridSize[0].val(); i > 0; i--) {
+	for (let i = 1; i < gridHeight.val(); i++) {
 		pixelCanvas.append('<tr></tr>');
-		for (let j = gridSize[1].val(); j > 0; j--) {
-			pixelCanvas.children().last().append("<td></td>");
+		for (let j = 1; j<gridWidth(); j++) {
+			pixelCanvas.children().last().append("<td id='pixel-"+i+"-"+j+"'></td>");
 		}
 	}
-	// console.log($('tr').length);
-	// $('td').attr('height', (500/($('tr').length))+"vh" );
 }
-
-$("#sizePicker").submit(function(e) {
+$("#sizePicker").change(function(e) {
 	makeGrid();
-    e.preventDefault();
+	$('table tr td').css('border', ".5px solid grey")
+	setTimeout(function() {
+		$('table tr td').css('border', 'none');
+    }, 500);
 });
+
 
 
 $("#pixelCanvas").click(function(e) {
@@ -32,24 +37,26 @@ $("#pixelCanvas").click(function(e) {
 	// if the click is dragged across the grid, this function prevents the <tr> from changing color
 	if (pixelToChange.is('td')){
 		pixelToChange.css('background-color', nextColor());
+	// this allows you to create gradients across rows
 	}else if (pixelToChange.is('tr')){
-		// console.log("row");
 		pixelToChange.children().each(function() {
 			$(this).css('background-color', incrementDrag());
 		});
+	// creates full screen gradient
 	}else{
-		// console.log('table click');
 		pixelToChange.children().children().each(function() {
 			$(this).css('background-color', incrementDrag());
 		})
 	}
 });
 
+// slowly morphing color formula
+const colorWonk=$("#inputWonk");
 const draggingColor=[200,200,200];
 function incrementDrag() {
 	let hexDrag="#"
 	for (i=0; i<3; i++){
-		draggingColor[i]+=(Math.round(Math.random()*10) -5);
+		draggingColor[i]+=Math.round((2.154**colorWonk.val())*(Math.random() -0.5));
 		draggingColor[i]>255 ? draggingColor[i]=255 : draggingColor[i]<0 ? draggingColor[i]=0 : true;
 		dragFragment=draggingColor[i].toString(16)
 		hexDrag+= (dragFragment.length==1 ? "0"+dragFragment : dragFragment);
@@ -57,6 +64,7 @@ function incrementDrag() {
 	return hexDrag;
 }
 
+// drawing function
 $("#pixelCanvas").mouseover(function(e) {
 	let dragPixel = $(e.target);
 	if (drawingToggle){
@@ -64,15 +72,13 @@ $("#pixelCanvas").mouseover(function(e) {
 	}
 });
 
-
-
+// Random color formulas
 function nextColor() {
 	for (i=0; i<3; i++){
 		draggingColor[i]=(Math.floor(Math.random() * 255));
 	}
 	return incrementDrag()
 }
-
 function randomColor() {
 	let hexCode = "#";
 	for(i=0; i<3; i++){
@@ -80,36 +86,54 @@ function randomColor() {
 	}
     return hexCode
 }
-
 function randomHexFragment() {
     var fragment = (Math.floor(Math.random() * 255).toString(16));
     return fragment.length == 1 ? "0" + fragment : fragment;
 }
 
-
-
-
-$('body').mousemove(function() {
-	// console.log('MOVE');
-});
-
+// random range selector, use negative values to select between -x to x
+function randomPicker(randomRange) {
+	if (randomRange<0){
+		Math.round(2*randomRange*(Math.random()-0.5));
+	} else {
+		Math.round(randomRange*Math.random());
+	}
+}
+// key commands
 $(document).keypress(function (e) {
 	if (e.which==32){
-		makeGrid();
-	} else if (e.which==100){
 		drawingToggle = !drawingToggle;
-	} else if (e.which==104){
+	} else if (e.which==(99||67)){
+		makeGrid();
+	} else if (e.which==(104||72)){
 		if ($('#cover').css('display') !== "none"){
 			$('#cover').stop(true, false).slideUp(500);
 		}else{
 			$('#cover').slideDown(500);
 		}
-	} else if (e.which==103){
-		console.log("HEY")
+	} else if (e.which==(102||70)){
+		$("#filter").css("background-color", "rgba(")
+	} else if (e.which==(115||83)){
+		$("#controls").css('display')=="none" ? $("#controls").css("display", "block") : $("#controls").css('display', 'none');
+		$("#basics").css('display')=="none" ? $("#basics").css("display", "block") : $("#basics").css('display', 'none');
+	} else if (e.which==(103||71)){
         window.location.assign('https://github.com/MichaelManwaring/pixelart');
     }
 });
 
-
-$(makeGrid());
-
+// stop mobile use untill further development
+function mobileWarning() {
+	if(window.innerWidth <= 800
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+	) {
+    	$("#instructions").empty().append("<h2>Sorry, not yet available on mobile</h2>");
+	}else{
+		makeGrid();
+	}
+}
+$(mobileWarning());
