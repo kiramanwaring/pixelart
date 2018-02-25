bbf0bbb33f3e73d4840a02832a88c731f9845fd1
Michael C Manwaring <Michael.c.manwaring@gmail.com>
2018-02-25 17:26:30 -0500

    Gremlin added

--- designs.js
+++ designs.js
@@ -8,8 +8,12 @@ function gridWidth() {
 	return Math.round(stretchTable[gridStretch.val()-1]*gridHeight.val());
 }
 // When size is submitted by the user, call makeGrid()
-
+const appTitle=$("#app-title")
+const instructions=$("#instructions");
+const basics=$("#basics");
+const controls=$("#controls");
 const pixelCanvas=$('#pixelCanvas');
+const sizePicker=$("#sizePicker")
 let drawingToggle=true;
 
 function makeGrid() {
@@ -21,7 +25,7 @@ function makeGrid() {
 		}
 	}
 }
-$("#sizePicker").change(function(e) {
+sizePicker.change(function(e) {
 	makeGrid();
 	$('table tr td').css('border', ".5px solid grey")
 	setTimeout(function() {
@@ -52,16 +56,15 @@ $("#pixelCanvas").click(function(e) {
 
 // slowly morphing color formula
 const colorWonk=$("#inputWonk");
-const draggingColor=[230,180,190];
+const draggingColor=[255,200,100];
 function incrementDrag() {
 	let hexDrag="#"
 	for (i=0; i<3; i++){
-		draggingColor[i]+=Math.round((2.154**colorWonk.val())*(Math.random() -0.5));
+		draggingColor[i]+=Math.round((2.2**colorWonk.val())*(Math.random() -0.5));
 		draggingColor[i]>255 ? draggingColor[i]=255 : draggingColor[i]<0 ? draggingColor[i]=0 : true;
 		dragFragment=draggingColor[i].toString(16)
 		hexDrag+= (dragFragment.length==1 ? "0"+dragFragment : dragFragment);
 	}
-	console.log(draggingColor);
 	return hexDrag;
 }
 
@@ -71,8 +74,8 @@ $("#pixelCanvas").mouseover(function(e) {
 	if (drawingToggle){
 		dragPixel.css('background-color', incrementDrag());
 	}
-	if ($('#cover').css('display') !== "none"){
-		$('#cover').css('color', incrementDrag());
+	if (appTitle.css('display') !== "none"){
+		appTitle.css('color', incrementDrag());
 	}
 });
 
@@ -91,8 +94,26 @@ function randomColor() {
     return hexCode;
 }
 function randomHexFragment() {
-    var fragment = randomPicker(255).toString(16);
+    let fragment = randomPicker(255).toString(16);
     return fragment.length == 1 ? "0" + fragment : fragment;
+}
+
+let gremlinOn=false;
+let currentPixel=[Math.round(gridHeight.val()/2), Math.round(gridWidth()/2)];
+function theGremlin(times) {
+	let maxPixel=[gridHeight.val(), gridWidth()];
+	for (var j = times; j >= 0; j--) {
+		for (var i = 1; i >= 0; i--) {
+			currentPixel[i]+=randomPicker(-2);
+			if (currentPixel[i]>maxPixel[i]){
+				currentPixel[i]-=3;
+			} else if (currentPixel[i]<0){
+				currentPixel[i]+=3;
+			}
+		}
+		let gremlinPixel=$("#pixel-"+currentPixel[0]+"-"+currentPixel[1]);
+		gremlinPixel.css('background-color', incrementDrag());
+	}
 }
 
 // random range selector, use negative values to select between -x to x
@@ -110,25 +131,35 @@ $(document).keypress(function (e) {
 	} else if (e.which==(99||67)){
 		makeGrid();
 	} else if (e.which==(105||73)){
-		if ($('#instructions').css('display') !== "none"){
-			$('#instructions').stop(true, false).slideUp(750);
+		if (instructions.css('display') !== "none"){
+			instructions.stop(true, false).slideUp(750);
 		}else{
-			$('#instructions').slideDown(750);
+			instructions.slideDown(750);
 		}
 	} else if (e.which==(104||72)){
-		if ($('#cover').css('display') !== "none"){
-			$('#cover').stop(true, false).hide();
+		if (appTitle.css('display') !== "none"){
+			appTitle.stop(true, false).hide();
 		}else{
-			$('#cover').show();
+			appTitle.show();
 		}
 	} else if (e.which==(102||70)){
-		$("#filter").css("background-color", "rgba(")
+		$('table').css("filter", "grayscale("+randomPicker(100)+"%)");
+	} else if (e.which==(119||87)){
+		if (gremlinOn) {
+			stopInterval();
+		} else {
+			setInterval(function(){
+				theGremlin(100);
+			}, 100);
+		}
+		gremlinOn=!gremlinOn;
 	} else if (e.which==(115||83)){
-		$("#controls").css('display')=="none" ? $("#controls").css("display", "block") : $("#controls").css('display', 'none');
-		$("#basics").css('display')=="none" ? $("#basics").css("display", "block") : $("#basics").css('display', 'none');
+		controls.css('display')=="none" ? controls.css("display", "block") : controls.css('display', 'none');
+		basics.css('display')=="none" ? basics.css("display", "block") : basics.css('display', 'none');
 	} else if (e.which==(103||71)){
         window.location.assign('https://github.com/MichaelManwaring/pixelart');
     }
+  	console.log(e.which)
 });
 
 
@@ -142,14 +173,14 @@ function mobileWarning() {
 		|| navigator.userAgent.match(/BlackBerry/i)
 		|| navigator.userAgent.match(/Windows Phone/i)
 	) {
-    	$("#instructions").empty().append("<h2>Sorry, not yet available on mobile</h2>");
+    	instructions.empty().append("<h2>Sorry, not yet available on mobile</h2>");
 	}else{
 		makeGrid();
 	}
 }
 $(mobileWarning());
 $(function() {
-	$('#instructions').delay(7500).slideDown(1000);
-	$('#cover').css('color', incrementDrag()).fadeIn(1000).fadeOut(16000);
+	instructions.delay(7500).slideDown(2000);
+	appTitle.css('color', incrementDrag()).fadeIn(1000).fadeOut(6000);
 
 })
